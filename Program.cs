277fafba6583d078763c099d00ecc80ce59e8673
miusarname <oscarm.alvarezg@gmail.com>
2023-11-08 19:43:23 -0500

    feat: :tada: Mi primera vez

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Oscar";
+int age = 18;
+const bool estaRico = true;
+double sueldo = 1500.101;
+
+Console.WriteLine($"Hola {name}, tengo {age} años y soy {estaRico} y a esa edas gano {sueldo}");
+Console.WriteLine("Hello, World!");
