98e066c8d69b7194edf8d5b18d4ee6e3073b5c91
miusarname <oscarm.alvarezg@gmail.com>
2023-11-13 15:40:03 -0500

    feat: :sparkles: Se estudia la programacion basica en C#

---
+++ Person.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mi_primera_vez
+{
+    public class Person
+    {
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,30 @@
 string name = "Oscar";
 int age = 18;
 const bool estaRico = true;
+
 double sueldo = 1500.101;
 
 Console.WriteLine($"Hola {name}, tengo {age} años y soy {estaRico} y a esa edas gano {sueldo}");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Dame tu nombre colega:");
+string? nombre = Console.ReadLine();
+Console.WriteLine($"Tu nombre es {nombre} y eres un amigo...");
+
+if ( nombre != "Juan")
+{
+    Console.WriteLine("Eres una persona ejemplar...");
+}
+else
+{
+    Console.WriteLine($"{nombre}, lo mama");
+}
+
+
+int[] Edades = new int[5];
+String[] Nombres= new String[] { "Juan","Felipe","Julian","Lo maman","putio"};
+int[] Algo = { 1, 2, 3, 4, 5, 6 };
+
+Console.WriteLine(Edades[4]);
+Console.WriteLine(Algo[3]);
+
+
