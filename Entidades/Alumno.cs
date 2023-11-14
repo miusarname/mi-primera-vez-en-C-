43827d751e43271805bf25c2c1d43fba3d2cdff8
miusarname <oscarm.alvarezg@gmail.com>
2023-11-14 08:37:00 -0500

    feat: :sparkles: Se "culmina" el aprendizaje de C#

--- Entidades/Alumno.cs
+++ Entidades/Alumno.cs
@@ -5,13 +5,68 @@ using System.Threading.Tasks;
 
 namespace mi_primera_vez.Entidades
 {
-    public class Alumno
+    public class Alumno : Person 
     {
-        private string ? idAlumno;
-        protected string? name;
-        protected int ? age;
-        public string? Apariencia;
+        private string? idAlumno;
+        protected string? email;
+        public string? apariencia;
+        public string? grupo;
 
-        public Alumno()=>idAlumno = Guid.NewGuid().ToString();
+
+        public Alumno() => idAlumno = Guid.NewGuid().ToString();
+
+        public string? IdAlumno
+        {
+            get
+            {
+                return this.idAlumno;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set { this.name = value; }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+
+            set { this.email = value; }
+        }
+
+        public string Apariencia
+        {
+            get
+            {
+                return this.apariencia;
+            }
+
+            set
+            {
+                this.apariencia = value;
+            }
+        }
+
+        public string Grupo
+        {
+            get
+            {
+                return this.grupo;
+            }
+
+            set 
+            { 
+            this.grupo = value;
+            }
+        }
     }
 }
--- Entidades/Person.cs
+++ Entidades/Person.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace mi_primera_vez
+namespace mi_primera_vez.Entidades
 {
     public class Person
     {
